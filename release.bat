a9d9fd644005412f3e25f014c7cedb744b517916
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-07-23 20:08:18 +0300

    Release scripts

---
+++ release.bat
@@ -0,0 +1,17 @@
+@echo off
+
+cd /d "%~dp0"
+
+if defined ConEmuPID (
+  call ConEmuC -GuiMacro Progress 3
+)
+
+call "%VS140COMNTOOLS%vsvars32.bat"
+
+cd
+
+msbuild WebInstall.vcxproj /nologo /t:Build /p:Configuration=Release;Platform=x86 /m:2
+
+if defined ConEmuPID (
+  call ConEmuC -GuiMacro Progress 0
+)
